d035ec10d388e48b5be17658d421835438f0e72e
RomanDeboi <80827446+RomanDeboi@users.noreply.github.com>
2026-09-24 06:58:33 +0300

    Delete tests/supabase.mock.d.ts

--- tests/supabase.mock.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@supabase/supabase-js' {
-  export interface SupabaseClient {
-    from(table: string): any;
-    rpc(name: string, args: Record<string, unknown>): Promise<any>;
-    storage: { from(bucket: string): any };
-  }
-}
